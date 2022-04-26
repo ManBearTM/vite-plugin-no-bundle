9e92e0431302d56ac4a2393bbe4ffe16281e60a2
ManBearTM <bjorn.id@bhoffmann.com>
2022-04-26 15:28:16 +0200

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,127 @@
+import path from 'path';
+import fs from 'fs';
+import micromatch from 'micromatch';
+import fg from 'fast-glob';
+import { Plugin } from 'vite';
+
+/** Checks if the provided `id` refers to a node module. */
+function isNodeModule(id: string) {
+  // Relative and absolute paths will almost always be
+  // resolvable using the file system, so require.resolve
+  // is not a reliable method for those types of import.
+  if (id.startsWith('.') || path.isAbsolute(id)) {
+    return id.includes('/node_modules/');
+  }
+  
+  try {
+    // If the `id` is neither relative nor absolute, AND is
+    // resolvable by Node, then it has to be a node module.
+    require.resolve(id);
+    return true;
+  } catch (_) {
+    // Failing to resolve here could mean the `id` is
+    // meant to be resolved by a different plugin.
+    return false;
+  }
+}
+
+interface Config {
+  /** @see https://rollupjs.org/guide/en/#outputpreservemodulesroot */
+  root?: string;
+  /** @see https://rollupjs.org/guide/en/#outputentryfilenames */
+  fileNames?: string;
+  /** Glob(s) for marking files as external while copying them to the output. */
+  copy?: string | string[];
+  /** Glob(s) for marking files as non-external, preserving them in the output. */
+  internal?: string | string[];
+}
+
+export default function plugin(config?: Config): Plugin {
+  const preserveModulesRoot = config?.root ?? 'src';
+  const entryFileNames = config?.fileNames ?? '[name].js';
+
+  // Store the resolved absolute root path
+  let root: string;
+
+  // Create a matcher function from provided internal config (if any)
+  const isInternal = (file: string) =>
+    config?.internal ? micromatch.isMatch(file, config.internal) : false;
+
+  // Create a matcher function from provided copy config (if any)
+  const isCopyTarget = (file: string) =>
+    config?.copy ? micromatch.isMatch(file, config.copy) : false;
+
+  return {
+    name: 'no-bundle',
+    enforce: 'pre',
+    apply: 'build',
+
+    config(userConfig) {
+      const { entry } = userConfig.build?.lib || {};
+      if (!entry) throw new Error('Required field "build.lib.entry" could not be found');
+      return {
+        build: {
+          lib: {
+            entry,
+            formats: ['es'],
+          },
+          rollupOptions: {
+            output: {
+              preserveModules: true,
+              preserveModulesRoot,
+              entryFileNames,
+            },
+          },
+        },
+      };
+    },
+
+    configResolved(resolvedConfig) {
+      root = resolvedConfig.root;
+    },
+
+    async buildStart() {
+      if (config?.copy) {
+        const files = await fg(config.copy, { cwd: root });
+        files.forEach(file => {
+          this.emitFile({
+            type: 'asset',
+            source: fs.readFileSync(path.join(root, file)),
+            fileName: preserveModulesRoot ? path.relative(preserveModulesRoot, file) : file,
+          });
+        });
+      }
+    },
+
+    async resolveId(source, importer, options: any) {
+      if (options.isEntry) return null;
+      if (isInternal(source)) return null;
+      if (isNodeModule(source)) return { id: source, external: true };
+
+      // Treat absolute paths as starting from project root
+      const absolutePath = path.isAbsolute(source)
+        ? path.join(root, source)
+        : path.join(path.dirname(importer), source);
+
+      // Get the relative path starting from `root`
+      const relativePath = path.relative(root, absolutePath);
+
+      // Mark the source as external and with side effects if it matches a glob pattern,
+      // excluding it from the build. The file is then emitted manually in buildStart.
+      if (isCopyTarget(relativePath)) {
+        // Enforce relative path to avoid issues with preserveModulesRoot
+        const id = path.isAbsolute(source)
+          ? path.relative(path.dirname(importer), absolutePath)
+          : source;
+
+        return {
+          id,
+          external: true,
+          moduleSideEffects: true,
+        };
+      }
+
+      return null;
+    },
+  };
+}
